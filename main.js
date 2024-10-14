3f44ea92bc1578407bbfa1c6178f1c6662e7ab62
dhanushsaicoder <dhanushsai1467@gmail.com>
2024-10-14 16:20:04 +0530

    generates class strings , pushes into regular/sub arrays

--- main.js
+++ main.js
@@ -71,19 +71,41 @@ let dataArr = [
   }
 ];
 
-let form1= document.getElementById('dataForm1')
-let form2= document.getElementById('dataForm2')
-let form3= document.getElementById('dataForm3')
-let form4= document.getElementById('dataForm4')
-let form5= document.getElementById('dataForm5')
-let form6= document.getElementById('dataForm6')
-let form7= document.getElementById('dataForm7')
-let form8= document.getElementById('dataForm8')
+let regularArr = []
+let subArr = []
+
+function generateClassString(form){
+  let cls = form.class.value;
+  let section = form.option.value;
+  let Class = cls+section;
+  if(form.substitution.checked){
+    subArr.push(Class)
+    return
+  }
+  regularArr.push(Class)
+  
+  
+}
 
 
 function submitForm(){
-  console.log(form1.substitution.checked)  
 
+  let form1= document.getElementById('dataForm1')
+  let form2= document.getElementById('dataForm2')
+  let form3= document.getElementById('dataForm3')
+  let form4= document.getElementById('dataForm4')
+  let form5= document.getElementById('dataForm5')
+  let form6= document.getElementById('dataForm6')
+  let form7= document.getElementById('dataForm7')
+  let form8= document.getElementById('dataForm8')
+
+  let formArr = [form1,form2,form3,form4,form5,form6,form7,form8]
+
+  for(let i = 0;i<8;i++)
+    generateClassString(formArr[i]);
+  
+  console.log(regularArr,subArr)
+  
 }
 
 
