dd04557579cef670c00a7f70352c7b6f15af7336
dhanushsaicoder <dhanushsai1467@gmail.com>
2024-10-15 11:46:35 +0530

    formatted

--- main.js
+++ main.js
@@ -1,219 +1,199 @@
 // Declare the password
-let pass = '';
+let pass = "";
 let login = false;
 // Function to start the login process
 function start() {
-    var passEle = document.getElementById('passEle');
+  var passEle = document.getElementById("passEle");
 
-    if (passEle.value === pass) {
-        document.getElementById('loginDiv').style.display = 'none';
-        document.getElementById('btnsDiv').style.display = 'flex';
-    document.getElementById('mainHeader').style.display = 'flex'
-
-    } else {
-        passEle.value = '';
-        alert('Incorrect Password');
-    }
+  if (passEle.value === pass) {
+    document.getElementById("loginDiv").style.display = "none";
+    document.getElementById("btnsDiv").style.display = "flex";
+    document.getElementById("mainHeader").style.display = "flex";
+  } else {
+    passEle.value = "";
+    alert("Incorrect Password");
+  }
 }
 
 // Functions to show various sections
 function showEnter() {
-    document.getElementById('content').style.display = 'none';
-    document.getElementById('reportDiv').style.display = 'none';
-document.getElementById('mainHeader').style.display = 'none'
-    document.getElementById('enterDiv').style.display = 'block';
+  document.getElementById("content").style.display = "none";
+  document.getElementById("reportDiv").style.display = "none";
+  document.getElementById("mainHeader").style.display = "none";
+  document.getElementById("enterDiv").style.display = "block";
 }
 
 function showReport() {
-    document.getElementById('content').style.display = 'none';
-    document.getElementById('enterDiv').style.display = 'none';
-    document.getElementById('reportDiv').style.display = 'block';
-document.getElementById('mainHeader').style.display = 'none'
-
+  document.getElementById("content").style.display = "none";
+  document.getElementById("enterDiv").style.display = "none";
+  document.getElementById("reportDiv").style.display = "block";
+  document.getElementById("mainHeader").style.display = "none";
 }
 
 // Function to go back to the main content
 function back(curr) {
-    if (curr === 'report') {
-document.getElementById('mainHeader').style.display = 'flex'
-        document.getElementById('reportDiv').style.display = 'none';
-        document.getElementById('content').style.display = 'flex';
-        showMenu();
-    } else if (curr === 'enter') {
-        document.getElementById('enterDiv').style.display = 'none';
-document.getElementById('mainHeader').style.display = 'flex'
-        document.getElementById('content').style.display = 'flex';
-        showMenu();
-    } else if (curr === 'settings'){
-      document.getElementById('settingsDivContainer').style.display = 'none'
-      document.getElementById('content').style.display = 'flex';
-      document.getElementById('settingsBtn').style.display = 'flex';
-
-
-    }
+  if (curr === "report") {
+    document.getElementById("mainHeader").style.display = "flex";
+    document.getElementById("reportDiv").style.display = "none";
+    document.getElementById("content").style.display = "flex";
+    showMenu();
+  } else if (curr === "enter") {
+    document.getElementById("enterDiv").style.display = "none";
+    document.getElementById("mainHeader").style.display = "flex";
+    document.getElementById("content").style.display = "flex";
+    showMenu();
+  } else if (curr === "settings") {
+    document.getElementById("settingsDivContainer").style.display = "none";
+    document.getElementById("content").style.display = "flex";
+    document.getElementById("settingsBtn").style.display = "flex";
+  }
 }
-document.getElementById('mainHeader').style.display = 'none'
+document.getElementById("mainHeader").style.display = "none";
 
 // Function to show the menu
 function showMenu() {
-    document.getElementById('loginDiv').style.display = 'none';
-    document.getElementById('btnsDiv').style.display = 'flex';
+  document.getElementById("loginDiv").style.display = "none";
+  document.getElementById("btnsDiv").style.display = "flex";
 }
 
 // Date initialization
 
-let date = new Date().getDate();  // one
-document.getElementById('dateEle').innerHTML = `Date: ${date} / ${new Date().getMonth() + 1} / ${new Date().getFullYear()}`;
-
-
-
+let date = new Date().getDate(); // one
+document.getElementById("dateEle").innerHTML = `Date: ${date} / ${
+  new Date().getMonth() + 1
+} / ${new Date().getFullYear()}`;
 
 //Data Arr
 
-let dataArrSchema = [ 
+let dataArrSchema = [
   {
-    date: new Date().getDate(),  // two
+    date: new Date().getDate(), // two
     month: new Date().getMonth() + 1,
     year: new Date().getFullYear(),
-    
+
     regularClasses: [],
     substitutionClasses: [],
-    
+
     totalRegClasses: 0,
     totalSubClasses: 0,
-    totalClasses: 0
-  }
+    totalClasses: 0,
+  },
 ];
 
-let regularArr = []
-let subArr = []
+let regularArr = [];
+let subArr = [];
 
-function generateClassString(i,form,dayObj){
+function generateClassString(i, form, dayObj) {
   let cls = form.class.value;
   let section = form.option.value;
-  let Class = cls+section;
+  let Class = cls + section;
 
-  //temporarily pushed into regula/subArr 
+  //temporarily pushed into regula/subArr
 
-  
-
-  if(form.substitution.checked){
-    dayObj.substitutionClasses[i]=Class
-    return
+  if (form.substitution.checked) {
+    dayObj.substitutionClasses[i] = Class;
+    return;
   }
-  dayObj.regularClasses[i]= Class
-  
-  
+  dayObj.regularClasses[i] = Class;
 }
 
-
-function submitForm(){
-
+function submitForm() {
   let dayObj = getDayObj();
-  
-  dayObj.regularClasses = []
-  dayObj.substitutionClasses = []
 
+  dayObj.regularClasses = [];
+  dayObj.substitutionClasses = [];
 
-  let form1= document.getElementById('dataForm1')
-  let form2= document.getElementById('dataForm2')
-  let form3= document.getElementById('dataForm3')
-  let form4= document.getElementById('dataForm4')
-  let form5= document.getElementById('dataForm5')
-  let form6= document.getElementById('dataForm6')
-  let form7= document.getElementById('dataForm7')
-  let form8= document.getElementById('dataForm8')
+  let form1 = document.getElementById("dataForm1");
+  let form2 = document.getElementById("dataForm2");
+  let form3 = document.getElementById("dataForm3");
+  let form4 = document.getElementById("dataForm4");
+  let form5 = document.getElementById("dataForm5");
+  let form6 = document.getElementById("dataForm6");
+  let form7 = document.getElementById("dataForm7");
+  let form8 = document.getElementById("dataForm8");
 
-  let formArr = [form1,form2,form3,form4,form5,form6,form7,form8]
+  let formArr = [form1, form2, form3, form4, form5, form6, form7, form8];
 
-  for(let i = 0;i<8;i++)
-    generateClassString(i,formArr[i],dayObj);
-  
-  console.log(dayObj)
+  for (let i = 0; i < 8; i++) generateClassString(i, formArr[i], dayObj);
+
+  console.log(dayObj);
 
   //send to local storage >>
 
-  let dataArr = JSON.parse(localStorage.getItem('dataArr'))
-  let dataArrLength= dataArr.length;
+  let dataArr = JSON.parse(localStorage.getItem("dataArr"));
+  let dataArrLength = dataArr.length;
 
-  if(dataArr[dataArrLength-1].date == date)
-    dataArr[dataArrLength-1] = dayObj;
-  else{
-    dataArr.push(dayObj)
-
+  if (dataArr[dataArrLength - 1].date == date)
+    dataArr[dataArrLength - 1] = dayObj;
+  else {
+    dataArr.push(dayObj);
   }
-  localStorage.setItem('dataArr',JSON.stringify(dataArr))
+  localStorage.setItem("dataArr", JSON.stringify(dataArr));
 
-
-  document.getElementById('enterContentDiv').style.display = 'none'
-  document.getElementById('successDiv').style.display = 'flex'
-
-
-  
+  document.getElementById("enterContentDiv").style.display = "none";
+  document.getElementById("successDiv").style.display = "flex";
 }
 
-document.getElementById('successDiv').style.display = 'none'
+document.getElementById("successDiv").style.display = "none";
 
 function getDayObj() {
-  if (typeof localStorage !== 'undefined') {
-    if (localStorage.getItem('dataArr') === null) {
-      let arr = [{
-        date: new Date().getDate(),    // three
-        month: new Date().getMonth() + 1,
-        year: new Date().getFullYear(),
-        regularClasses: [],
-        substitutionClasses: [],
-        totalRegClasses: 0,
-        totalSubClasses: 0,
-        totalClasses: 0
-      }];
-      localStorage.setItem('dataArr', JSON.stringify(arr));
+  if (typeof localStorage !== "undefined") {
+    if (localStorage.getItem("dataArr") === null) {
+      let arr = [
+        {
+          date: new Date().getDate(), // three
+          month: new Date().getMonth() + 1,
+          year: new Date().getFullYear(),
+          regularClasses: [],
+          substitutionClasses: [],
+          totalRegClasses: 0,
+          totalSubClasses: 0,
+          totalClasses: 0,
+        },
+      ];
+      localStorage.setItem("dataArr", JSON.stringify(arr));
     }
-    let dataArr = JSON.parse(localStorage.getItem('dataArr'));
+    let dataArr = JSON.parse(localStorage.getItem("dataArr"));
     let dataArrLength = dataArr.length;
-    
-    
-    let today = new Date().getDate();  // four
 
+    let today = new Date().getDate(); // four
 
     if (dataArr[dataArrLength - 1].date === today) {
       return dataArr[dataArrLength - 1];
     } else {
       let newDayObj = {
-        date: today,      
+        date: today,
         month: new Date().getMonth() + 1,
         year: new Date().getFullYear(),
         regularClasses: [],
         substitutionClasses: [],
         totalRegClasses: 0,
         totalSubClasses: 0,
-        totalClasses: 0
+        totalClasses: 0,
       };
       dataArr.push(newDayObj);
-      localStorage.setItem('dataArr', JSON.stringify(dataArr));
+      localStorage.setItem("dataArr", JSON.stringify(dataArr));
       return newDayObj;
     }
   } else {
-    console.error('Local storage is not supported in this browser.');
+    console.error("Local storage is not supported in this browser.");
     return null;
   }
 }
 
-document.getElementById('settingsDivContainer').style.display='none'
+document.getElementById("settingsDivContainer").style.display = "none";
 
-function reEnter(){
-   document.getElementById('enterContentDiv').style.display = 'flex'
-  document.getElementById('successDiv').style.display = 'none'
+function reEnter() {
+  document.getElementById("enterContentDiv").style.display = "flex";
+  document.getElementById("successDiv").style.display = "none";
 }
 
-function openSettings(){
-
-  document.getElementById('content').style.display='none'
-  document.getElementById('settingsBtn').style.display='none'
-  document.getElementById('settingsDivContainer').style.display='flex'
-
+function openSettings() {
+  document.getElementById("content").style.display = "none";
+  document.getElementById("settingsBtn").style.display = "none";
+  document.getElementById("settingsDivContainer").style.display = "flex";
 }
-function resetData(){
-  localStorage.removeItem('dataArr')
-  window.location.href = 'index.html';
+function resetData() {
+  localStorage.removeItem("dataArr");
+  window.location.href = "index.html";
 }
